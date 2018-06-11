af9fb7ac41a77b9087b526c58f8abc77b562bc70
Vladimir Ivanov <vladimir.ivanov@lazada.com>
2018-06-11 10:35:53 +0800

    Log user id via syslog() whe user tries to login interactively

--- forsh.c
+++ forsh.c
@@ -1,7 +1,16 @@
 #include <sys/types.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <syslog.h>
 
 int main(int argc, char **argv)
 {
+  if (isatty(fileno(stdout))) {
+    openlog("forsh", LOG_PID, LOG_AUTH);
+    syslog(LOG_NOTICE, "user %d attempted to login interactively", getuid());
+    closelog();
+    puts ("You are not allowed to login inteactively to this system. This incident will be reported.");
+  }
   return EXIT_SUCCESS;
 }
